90b4a3695e25c8b83157bfc4ec6373501c66a92b
Christian Di Lorenzo <rcdilorenzo@me.com>
2013-03-03 18:44:30 -0500

    Add Matthew 3 and add coming soon to rest of chapters

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,10 +2,6 @@ class NotesController < ApplicationController
   def index
   end
 
-  def get_chapter
-    redirect_to :action => "chapter", :chapter => params[:chapter]
-  end
-
   def chapter
     @current_chapter = Chapter.find_by_number params[:chapter]
     render :layout => "notes_with_sidebar"
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Matthewnotes::Application.routes.draw do
 
   root :to => "notes#index"
   match "chapter/:chapter" => "notes#chapter", :as => :chapters
-  match "chapter" => "notes#get_chapter"
+  match "chapter" => "notes#chapter"
 
 end
